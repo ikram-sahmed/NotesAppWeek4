0541c7a96a1acf6347b0896c1620b18558aeb0c5
Ikram Ahmed <ikramsadiq200@gmail.com>
2022-10-21 13:11:30 -0500

    completed

--- noteApp/src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
+++ noteApp/src/main/java/com/devmountain/noteApp/services/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
         List<String> response = new ArrayList<>();
         User user = new User(userDto);
         userRepository.saveAndFlush(user);
-        response.add("User Added Successfully!");
+        response.add("http://http://localhost:8080/login.html");
         return response;
     }
     @Override
@@ -34,7 +34,7 @@ public class UserServiceImpl implements UserService {
         Optional<User> userOptional = userRepository.findByusername(userDto.getUsername());
         if (userOptional.isPresent()) {
             if (passwordEncoder.matches(userDto.getPassword(), userOptional.get().getPassword())) {
-                response.add("User Login Successful");
+                response.add("http://http://localhost:8080/home.html");
                 response.add(String.valueOf(userOptional.get().getId()));
             } else {
                 response.add("Username or password incorrect");
